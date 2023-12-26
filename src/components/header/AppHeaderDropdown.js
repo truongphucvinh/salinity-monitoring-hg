2f2a22c17985cba82e3fc4b54eb11660c2fb5fdf
Minh Bang <bradonleminhbang1@gmail.com>
2023-12-26 07:14:48 +0700

    SM_0007: Adding a authentication checker component

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   CAvatar,
   CBadge,
@@ -23,73 +23,101 @@ import {
 import CIcon from '@coreui/icons-react'
 
 import avatar8 from './../../assets/images/avatars/8.jpg'
+import { useNavigate } from 'react-router-dom'
 
 const AppHeaderDropdown = () => {
+  const [isAuthenticated, setIsAuthencticated] = useState(false)
+  const navigate = useNavigate()
+  const onUpdateAuthStatus = () => {
+    if (localStorage.getItem('_isAuthenticated')) {
+      setIsAuthencticated(true)
+    }else {
+      setIsAuthencticated(false)
+    }
+  }
+  const handleLogout = () => {
+    if (isAuthenticated) {
+      localStorage.removeItem('_isAuthenticated')
+      localStorage.removeItem('_authenticatedUser')
+      onUpdateAuthStatus()
+      navigate("/login")
+    }
+  }
+  useEffect(() => {
+    onUpdateAuthStatus()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <CDropdown variant="nav-item">
-      <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar8} size="md" />
-      </CDropdownToggle>
-      <CDropdownMenu className="pt-0" placement="bottom-end">
-        <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilBell} className="me-2" />
-          Updates
-          <CBadge color="info" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilEnvelopeOpen} className="me-2" />
-          Messages
-          <CBadge color="success" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilTask} className="me-2" />
-          Tasks
-          <CBadge color="danger" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilCommentSquare} className="me-2" />
-          Comments
-          <CBadge color="warning" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownHeader className="bg-body-secondary fw-semibold my-2">Settings</CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilUser} className="me-2" />
-          Profile
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilSettings} className="me-2" />
-          Settings
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilCreditCard} className="me-2" />
-          Payments
-          <CBadge color="secondary" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilFile} className="me-2" />
-          Projects
-          <CBadge color="primary" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownDivider />
-        <CDropdownItem href="#">
-          <CIcon icon={cilLockLocked} className="me-2" />
-          Lock Account
-        </CDropdownItem>
-      </CDropdownMenu>
-    </CDropdown>
+    <>
+      {
+        isAuthenticated ? <CDropdown variant="nav-item">
+          <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
+            <CAvatar src={avatar8} size="md" />
+          </CDropdownToggle>
+          <CDropdownMenu className="pt-0" placement="bottom-end">
+            {/* <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
+            <CDropdownItem href="#">
+              <CIcon icon={cilBell} className="me-2" />
+              Updates
+              <CBadge color="info" className="ms-2">
+                42
+              </CBadge>
+            </CDropdownItem>
+            <CDropdownItem href="#">
+              <CIcon icon={cilEnvelopeOpen} className="me-2" />
+              Messages
+              <CBadge color="success" className="ms-2">
+                42
+              </CBadge>
+            </CDropdownItem>
+            <CDropdownItem href="#">
+              <CIcon icon={cilTask} className="me-2" />
+              Tasks
+              <CBadge color="danger" className="ms-2">
+                42
+              </CBadge>
+            </CDropdownItem>
+            <CDropdownItem href="#">
+              <CIcon icon={cilCommentSquare} className="me-2" />
+              Comments
+              <CBadge color="warning" className="ms-2">
+                42
+              </CBadge>
+            </CDropdownItem> */}
+            {/* <CDropdownHeader className="bg-body-secondary fw-semibold my-2">Settings</CDropdownHeader> */}
+            {/* <CDropdownItem href="#">
+              <CIcon icon={cilUser} className="me-2" />
+              Profile
+            </CDropdownItem>
+            <CDropdownItem href="#">
+              <CIcon icon={cilSettings} className="me-2" />
+              Settings
+            </CDropdownItem>
+            <CDropdownItem href="#">
+              <CIcon icon={cilCreditCard} className="me-2" />
+              Payments
+              <CBadge color="secondary" className="ms-2">
+                42
+              </CBadge>
+            </CDropdownItem>
+            <CDropdownItem href="#">
+              <CIcon icon={cilFile} className="me-2" />
+              Projects
+              <CBadge color="primary" className="ms-2">
+                42
+              </CBadge>
+            </CDropdownItem> */}
+            <CDropdownDivider />
+            <CDropdownItem href={false} onClick={handleLogout}>
+              <CIcon icon={cilLockLocked}  className="me-2" />
+              Đăng xuất
+            </CDropdownItem>
+          </CDropdownMenu>
+          </CDropdown> : ''
+      }
+    </>
+
   )
 }
 
