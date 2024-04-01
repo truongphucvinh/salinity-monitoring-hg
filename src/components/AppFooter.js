b7f33e1b496a764b5a0c7d1f7e5aadafe3de99cd
Minh Bang <bradonleminhbang1@gmail.com>
2024-04-01 20:49:22 +0700

    SM_0018: Adding permission checker for the update method of general information

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import { CFooter } from '@coreui/react'
+import React, { useState } from 'react'
+import { CButton, CFooter } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilPencil } from '@coreui/icons'
+import CustomAuthorizationCheckerChildren from 'src/views/customs/my-authorizationchecker-children'
 
 const AppFooter = () => {
+  const defaultAuthorizationCode = process.env.HG_MODULE_GENERAL_INFORMATION_MANAGEMENT || "U2FsdGVkX1/CWjVqRRnlyitZ9vISoCgx/rEeZbKMiLQ=_general_information_management"
+  // Checking feature's module
+  const defaultModuleUpdateFooter = "U2FsdGVkX1/CWjVqRRnlyitZ9vISoCgx/rEeZbKMiLQ=_general_information_management_update_footer_information"
+  const [havingUpdateFooter, setHavingUpdateFooter] = useState(false)
   return (
     <CFooter className="px-4">
+      <CustomAuthorizationCheckerChildren parentCode={defaultAuthorizationCode} checkingCode={defaultModuleUpdateFooter} setExternalState={setHavingUpdateFooter}/>
       <div>
         {/* <a href="http://se.cit.ctu.edu.vn/" target="_blank" rel="noopener noreferrer">
           SE, CIT
@@ -16,6 +24,14 @@ const AppFooter = () => {
           Khoa Kỹ thuật Phần mềm (SE), Trường CNTT&TT (CIT), Trường Đại học Cần Thơ (CTU)
         </a>
       </div>
+        {
+          havingUpdateFooter && <div className='ms-3'>
+          <CButton className='btn btn-primary'>
+            <CIcon icon={cilPencil} className='me-2'/>
+            Cập nhật
+          </CButton>
+        </div>
+        }
     </CFooter>
   )
 }
